754ee80dbaf65e1a11720885f431ab412c2c93f1
lijing <lijingkf@codemao.cn>
2020-08-24 17:53:30 +0800

    使用参数方程绘制各种曲线

---
+++ 2-2.使用参数方程快速绘制各种曲线/app.js
@@ -0,0 +1,43 @@
+import {parametric} from '../common/lib/parametric.js';
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const {width, height} = canvas;
+const w = 0.5 * width,
+  h = 0.5 * height;
+ctx.translate(w, h);
+ctx.scale(1, -1);
+
+function drawAxis() {
+  ctx.save();
+  ctx.strokeStyle = '#ccc';
+  ctx.beginPath();
+  ctx.moveTo(-w, 0);
+  ctx.lineTo(w, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(0, -h);
+  ctx.lineTo(0, h);
+  ctx.stroke();
+  ctx.restore();
+}
+// 绘制 XY 轴
+drawAxis();
+// 绘制抛物线
+const para = parametric(
+  t => 25 * t,
+  t => 25 * t ** 2,
+);
+para(-5.5, 5.5).draw(ctx);
+// 绘制 阿基米德螺旋线
+const helical = parametric(
+  (t, l) => l * t * Math.cos(t),
+  (t, l) => l * t * Math.sin(t),
+);
+helical(0, 50, 500, 5).draw(ctx, {strokeStyle: 'blue'});
+// 绘制 星形线
+const star = parametric(
+  (t, l) => l * Math.cos(t) ** 3,
+  (t, l) => l * Math.sin(t) ** 3,
+);
+star(0, Math.PI * 2, 50, 150).draw(ctx, {strokeStyle: 'red'});
